d296eba9a70f1abafd1910ee1eb9911b87b42299
Sri Hari Lakshmanan <sriharilakshmanan05@gmail.com>
2025-05-11 22:35:44 +0530

    feat: add create-topic-form and create-topic action to create a topic and save it

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,71 @@
 'use server';
 
-export async function createTopic() {
-    // TODO: revalidate home page
+import type { Topic } from '@prisma/client';
+import { z } from 'zod';
+import { auth } from '@/auth';
+import { redirect } from 'next/navigation';
+import { db } from '@/db';
+import paths from '@/paths';
+import { revalidatePath } from 'next/cache';
+
+const createTopicSchema = z.object({
+    name: z
+        .string()
+        .min(2, 'Must be at least 2 characters long')
+        .regex(/[a-z-]/, 'Must be lowercase and hyphenated'),
+    description: z.string().min(10, 'Must be at least 10 characters long'),
+});
+
+interface CreateTopicFormState {
+    errors: { name?: string[]; description?: string[]; _form?: string[] };
+}
+
+export async function createTopic(
+    formState: CreateTopicFormState,
+    formData: FormData
+): Promise<CreateTopicFormState> {
+    const session = await auth();
+    if (!session || !session.user) {
+        return {
+            errors: {
+                _form: ['You must be signed in to create a topic'],
+            },
+        };
+    }
+
+    const name = formData.get('name');
+    const description = formData.get('description');
+
+    const result = createTopicSchema.safeParse({ name, description });
+    if (!result.success) {
+        return {
+            errors: result.error.flatten().fieldErrors,
+        };
+    }
+
+    let topic: Topic;
+    try {
+        topic = await db.topic.create({
+            data: {
+                slug: result.data.name,
+                description: result.data.description,
+            },
+        });
+    } catch (error) {
+        if (error instanceof Error) {
+            return {
+                errors: {
+                    _form: [error.message],
+                },
+            };
+        }
+        return {
+            errors: {
+                _form: ['Something went wrong. Please try again later.'],
+            },
+        };
+    }
+
+    revalidatePath(paths.home());
+    redirect(paths.topicShow(topic.slug));
 }
---
+++ src/components/topics/topic-create-form.tsx
@@ -0,0 +1,66 @@
+'use client';
+import {
+    Button,
+    Input,
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+    Textarea,
+} from '@nextui-org/react';
+import * as actions from '@/actions';
+import { startTransition, useActionState } from 'react';
+
+export default function TopicCreateForm() {
+    const [formState, action] = useActionState(actions.createTopic, {
+        errors: {},
+    });
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        startTransition(() => {
+            action(formData);
+        });
+    }
+    return (
+        <Popover placement="bottom" backdrop="opaque">
+            <PopoverTrigger>
+                <Button color="primary" radius="full">
+                    Create Topic
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent>
+                <form
+                    className="flex flex-col gap-4 p-4 w-80"
+                    onSubmit={handleSubmit}
+                    noValidate
+                >
+                    <h3 className="text-lg">Create Topic</h3>
+                    <Input
+                        name="name"
+                        label="Name"
+                        labelPlacement="outside"
+                        placeholder="Name the topic"
+                        isInvalid={!!formState.errors.name}
+                        errorMessage={formState.errors.name?.join(', ')}
+                    />
+                    <Textarea
+                        name="description"
+                        label="Description"
+                        labelPlacement="outside"
+                        placeholder="Describe the topic"
+                        isInvalid={!!formState.errors.description}
+                        errorMessage={formState.errors.description?.join(', ')}
+                    />
+                    {formState.errors._form ? (
+                        <p className="m-auto text-danger">
+                            {formState.errors._form.join(', ')}
+                        </p>
+                    ) : null}
+                    <Button type="submit" radius="full">
+                        Submit
+                    </Button>
+                </form>
+            </PopoverContent>
+        </Popover>
+    );
+}
