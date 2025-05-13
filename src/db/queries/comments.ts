e2a1d70784c163fdf856351271c800ae717a3f0e
Sri Hari Lakshmanan <sriharilakshmanan05@gmail.com>
2025-05-13 18:08:06 +0530

    fix: deduplicate queries by using request memoization

--- src/db/queries/comments.ts
+++ src/db/queries/comments.ts
@@ -1,5 +1,6 @@
 import type { Comment } from '@prisma/client';
 import { db } from '@/db';
+import { cache } from 'react';
 export type CommentWithAuthor = Comment & {
     user: {
         name: string | null;
@@ -7,21 +8,20 @@ export type CommentWithAuthor = Comment & {
     };
 };
 
-export function fetchCommentsByPostId(
-    postId: string
-): Promise<CommentWithAuthor[]> {
-    console.log('fetchCommentsByPostId: ', postId);
-    return db.comment.findMany({
-        where: {
-            postId,
-        },
-        include: {
-            user: {
-                select: {
-                    name: true,
-                    image: true,
+export const fetchCommentsByPostId = cache(
+    (postId: string): Promise<CommentWithAuthor[]> => {
+        return db.comment.findMany({
+            where: {
+                postId,
+            },
+            include: {
+                user: {
+                    select: {
+                        name: true,
+                        image: true,
+                    },
                 },
             },
-        },
-    });
-}
+        });
+    }
+);
